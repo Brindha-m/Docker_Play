0a9286f6a6d02141393a8ea9b9061da9da783f60
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-09-16 16:43:31 +0530

    Update app.py

--- Sampleapp/app.py
+++ Sampleapp/app.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("Let's learn about Docker Deck: Containers, Images, Ready, Set!")
+st.header("🐳Sample Docker App")
+st.caption("Let's learn about Docker Deck: Containers, Images, Ready, Set!😎")
+
+st.success("✅Successfully created First Docker Image!")
